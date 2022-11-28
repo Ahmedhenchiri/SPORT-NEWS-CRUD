d111b6982df64cd44758d751b1cdd11c0878a4ea
macbook air <macbookair@MacBook-Air-de-macbook.local>
2022-11-28 13:19:40 +0100

    add folder controllers

--- server/database/Model/sport-news.js
+++ server/database/Model/sport-news.js
@@ -2,7 +2,18 @@ const conn = require ('../index.js');
 
 module.exports={
 getAll:function(callback){
-    const sql = `SELECT `
+    const sql = "SELECT * FROM `sport`"
+    conn.query(sql,function(error,results){
+        callback(error,results)
+    })
+},
+
+add:function(news,callback){
+    const sql = "INSERT INTO `sport` SET ? "; 
+    conn.query(sql,news,function(error,results){
+        callback(error,results)
+    })
 }
+
 
 }
--- server/database/index.js
+++ server/database/index.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
-const config = require ('config');
+
 
 
 
 // create a databases connection and export it from this file 
 
-const connection= mysql.createConnection(config); 
+const connection= mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'sport-news'
+}); 
 
 connection.connect(function(err){
     if(err)console.log('Error to connect to database ',err)
