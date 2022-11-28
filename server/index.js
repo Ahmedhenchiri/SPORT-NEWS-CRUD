e650a4d445d60a93359c32d45e24677a7861ae3c
macbook air <macbookair@MacBook-Air-de-macbook.local>
2022-11-28 14:01:56 +0100

    fix routes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 
+const app = require('./app') // import app file 
+const db = require ('./database') // import databases folder 
+
+
+const port = 5000;//setting the port and listening for conection 
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`your server lesteing in port ${port}`)
+})
--- server/routes/sport-news.js
+++ server/routes/sport-news.js
@@ -1,2 +1,24 @@
 const express = require('express'); 
 
+const router = express.Router(); 
+
+const {getAllSport,addSport,deleteSport,updateSport} = require('../controllers/sport-news')
+
+//GET request to fetch all sport 
+router.get('/getAll',getAllSport)
+
+//POST reqest to add new news 
+router.post('/add',addSport)
+
+//DELTE REQEST to delete news 
+router.delete('/delete/:id',deleteSport)
+
+//updata reqest to modfier news 
+router.put('/update/:id',updateSport)
+
+
+
+
+
+
+module.exports= router ; 
